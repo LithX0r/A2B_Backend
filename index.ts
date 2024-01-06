aced40876d09c202f99d1613fe2006acc0252537
mtasd1 <m.tasdemir2000@hotmail.com>
2024-01-06 16:32:15 +0100

    delete project and make new setup

--- index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-const app = express();
-const port: number = 3000;
-app.get('/', (req, res) => {
-	res.send("test");
-});
-app.listen(port, () => {console.log(`Server is running on port ${port}`)});
