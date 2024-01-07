4574f1dbcef004e5a1f65d85351ed7cfc4e5039b
mtasd1 <m.tasdemir2000@hotmail.com>
2024-01-07 16:58:22 +0100

    implement GET for rider and driver

--- src/routes/driverRoutes.ts
+++ src/routes/driverRoutes.ts
@@ -1,6 +1,31 @@
 import {Router} from "express";
+import {AppDataSource} from "../data-source";
+import {Driver} from "../entity/Driver";
 
 const driverRoutes = Router();
+
+driverRoutes.get("/:driverId", async function (req, res) {
+    try {
+        const driverId = parseInt(req.params.driverId, 10);
+        console.log(`driverId: ${driverId}`)
+
+        if (isNaN(driverId)) {
+            res.status(400).json({ message: "Invalid driverId provided" });
+            return;
+        }
+
+        const driver = await AppDataSource.getRepository(Driver).findOneBy({id: driverId});
+        console.log(`driver: ${driver}`)
+
+        if (driver != null) {
+            res.json(driver);
+        } else {
+            res.status(404).json({message: `Driver with id not found`});
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
 
 
 
--- src/routes/riderRoutes.ts
+++ src/routes/riderRoutes.ts
@@ -5,7 +5,26 @@ import {AppDataSource} from "../data-source";
 const riderRoutes = Router();
 
 riderRoutes.get("/:riderId", async function (req, res) {
+    try {
+        const riderId = parseInt(req.params.riderId, 10);
+        console.log(`riderId: ${riderId}`)
 
+        if (isNaN(riderId)) {
+            res.status(400).json({ message: "Invalid riderId provided" });
+            return;
+        }
+
+        const rider = await AppDataSource.getRepository(Rider).findOneBy({id: riderId});
+        console.log(`rider: ${rider}`)
+
+        if (rider != null) {
+            res.json(rider);
+        } else {
+            res.status(404).json({message: `Rider with id not found`});
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 })
 
 export default riderRoutes;
